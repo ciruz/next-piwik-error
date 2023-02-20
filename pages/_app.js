82e50f44f7ed7c76e5285f500fcfe72bfd8d4b4c
ciruz <dietrich.tom@gmail.com>
2023-02-20 12:50:16 +0100

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
 
+import PiwikProProvider from '@piwikpro/next-piwik-pro'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <PiwikProProvider
+      accountName='accountName'
+      containerId='43e4bca4-e220-43df-acfc-40fef7e25105'>
+      <Component {...pageProps} />
+    </PiwikProProvider>
+  </>
 }
